b2d00414918ea82d5598bd869dd6b51f4dcd901c
DGorasia <38210632+DGorasia@users.noreply.github.com>
2018-05-31 11:53:03 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String[] main) {
 		System.out.println("Hello World");
-		System.out.println("Hello Devun, nice to meet you!");
+		System.out.println("Hello Devun!");
 	}
 }
